00202d06975e68bd121f2a1ad65e568840ba577b
Ronald Lipski Roderjan <roderjanronald1@gmail.com>
2025-10-03 21:18:32 -0300

    feat: Adiciona configuração de CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import categorias, pilotos, transacoes, dashboard
 
 app = FastAPI(
     title="RA KartRacing API",
     description="API para gestão financeira da equipe de kart RA KartRacing.",
     version="0.1.0"
+)
+
+origins = [
+    "http://localhost:5173",
+    "http://RA-Kart-Racing.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(categorias.router)
